dae2e040a8938bf8a053bad7a691fc8b906b44d7
perinova <ruzena05perinova@gmail.com>
2022-03-28 13:56:47 +0200

    02 - form

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 /* Nastavení portu, na němž bude spuštěný server naslouchat */
 const port = 3000;
 
+/* Identifikace složky obsahující statické soubory klientské části webu */
+app.use(express.static("public"));
+
 /* Spuštění webového serveru */
 app.listen(port, () => {
     console.log(`Server naslouchá na portu ${port}`);
 });
+
