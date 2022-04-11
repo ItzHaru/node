c482dbc0e1bf5450a810907dea3466966d4bfe4c
Haru <77784767+ItzHaru@users.noreply.github.com>
2022-04-11 21:37:55 +0200

    04 - oprava slozek a souboru

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-/* Připojení modulu frameworku Express */
-const express = require("express");
-
-/* Vytvoření základního objektu serverové aplikace */
+const express = require('express');
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+const csv = require('csvtojson');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+const port = 5000;
 
-/* Nastavení portu, na němž bude spuštěný server naslouchat */
-const port = 3000;
 
-/* Identifikace složky obsahující statické soubory klientské části webu */
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 /* Spuštění webového serveru */
-app.listen(port, () => {
-    console.log(`Server naslouchá na portu ${port}`);
+
+app.post('/savedata', urlencodedParser, (req, res) => {
+    let str = `${req.body.bias},${req.body.song},${req.body.album},${req.body.year}\n`;
+    fs.appendFile('./data/answers.csv', str, function(err) {
+        if (err) {
+            console.error(err);
+            return res.status(400).json({
+               success: false,
+               message: 'An error was encountered while writing to the file' 
+            });
+        }
+    });
+    res.redirect(301, '/savedata');
 });
 
+app.get('/savedata', (req, res) => {  
+    csv().fromFile(path.join(__dirname,'./data/answers.csv')).then(data => { 
+   res.render('answers.pug', {'form':data,'title': 'ARMY stats '});
+    })
+     .catch(err => {
+    console.log(err);
+    });
+    });
+
+app.listen(port, () => { 
+    console.log('Server naslouchá na portu 5000');
+    });
